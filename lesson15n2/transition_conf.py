0233cb9ff919e2189e571851629f551c08ee146e
むずでょ <muzudho1@gmail.com>
2021-12-14 18:42:10 +0900

    Lesson 15

--- lesson15n2/transition_conf.py
+++ lesson15n2/transition_conf.py
@@ -1,44 +1,19 @@
-# States
-# ------
-#
-# ディクショナリーのキーとして Edge と被らないように PascalCase にします
-
-INIT = "Init"
-if INIT:
-    THIS = "This"
-    if THIS:  # インデントを付けてるだけ
-        IS = "Is"
-        if IS:
-            A = "A"
-
-PEN = "Pen"
-
-# Actions
-# -------
-
-MSG_THAT = "That"
-MSG_THIS = "This"
-MSG_WAS = "was"
-MSG_IS = "is"
-MSG_AN = "an"
-MSG_A = "a"
-MSG_PIN = "pin"
-MSG_PEN = "pen"
-
-# Edges
-# -----
-#
-# ディクショナリーのキーとして State と被らないように頭に snake_case にします
-
-E_THAT = "that"
-E_THIS = "this"
-E_WAS = "was"
-E_IS = "is"
-E_AN = "an"
-E_A = "a"
-E_PIN = "pin"
-E_PEN = "pen"
-E_OVER = "over"  # 以上
+from lesson15n2.const_conf import (
+    A,
+    E_A,
+    E_AN,
+    E_IS,
+    E_OVER,
+    E_PEN,
+    E_PIN,
+    E_THAT,
+    E_THIS,
+    E_WAS,
+    INIT,
+    IS,
+    PEN,
+    THIS,
+)
 
 
 class TransitionConf:
