6716cdd55021e87c62cddfbf89102f983b6b3501
むずでょ <muzudho1@gmail.com>
2021-12-14 22:41:41 +0900

    Replace

--- lesson18/code_gen/main_step2.py
+++ lesson18/code_gen/main_step2.py
@@ -1,24 +1,26 @@
 import sys
 
-from lesson07.main_and_finally import MainAndFinally
+from lesson07n2.main_finally import MainFinally
 from lesson18.code_gen.render_step2 import Render
 
 server = None
 
 
-def __on_main():
-    server = Render()
-    server.run()
+class Main:
+    def on_main(self):
+        server = Render()
+        server.run()
+        return 0
 
+    def on_finally(self):
+        # [Ctrl] + [C] を受け付けないから、ここにくるのは難しい
+        if server:
+            server.clean_up()
 
-def __on_finally():
-    # [Ctrl] + [C] を受け付けないから、ここにくるのは難しい
-    if server:
-        server.clean_up()
-
-    print("★しっかり終わった")
+        print("★しっかり終わった")
+        return 1
 
 
 # このファイルを直接実行したときは、以下の関数を呼び出します
 if __name__ == "__main__":
-    sys.exit(MainAndFinally(on_main=__on_main, on_finally=__on_finally).run())
+    sys.exit(MainFinally.run(Main()))
