ccdb1307b7eee9ab233aa2b4bd678f181a47d172
むずでょ <muzudho1@gmail.com>
2021-12-14 22:18:04 +0900

    Bug fix

--- lesson12n2/main.py
+++ lesson12n2/main.py
@@ -1,24 +1,24 @@
 import sys
 
-from lesson07.main_and_finally import MainAndFinally
+from lesson07n2.main_finally import MainFinally
 from lesson12n2.server import Server
 
 server = None
 
 
-def __on_main():
-    server = Server(host="0.0.0.0", port=5002)
-    server.run()
+class Main:
+    def on_main(self):
+        server = Server(host="0.0.0.0", port=5002)
+        server.run()
 
+    def on_finally(self):
+        # [Ctrl] + [C] を受け付けないから、ここにくるのは難しい
+        if server:
+            server.clean_up()
 
-def __on_finally():
-    # [Ctrl] + [C] を受け付けないから、ここにくるのは難しい
-    if server:
-        server.clean_up()
-
-    print("★しっかり終わった")
+        print("★しっかり終わった")
 
 
 # このファイルを直接実行したときは、以下の関数を呼び出します
 if __name__ == "__main__":
-    sys.exit(MainAndFinally(on_main=__on_main, on_finally=__on_finally).run())
+    sys.exit(MainFinally.run(Main()))
--- lesson12n2/server.py
+++ lesson12n2/server.py
@@ -3,7 +3,7 @@ from threading import Thread
 
 from lesson12n2.states.out import OutState
 from lesson12n2.step3_state_gen_conf import state_gen
-from lesson12n2.step1_const_conf_house import OUT
+from lesson12.step1_const_conf_house_v3 import OUT
 from lesson12n2.step2_transition_conf_house import transition_conf
 
 
