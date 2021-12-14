1a791c64b37cd69057d82d2213e7e239982d00cb
むずでょ <muzudho1@gmail.com>
2021-12-14 18:36:25 +0900

    const_conf

--- lesson16/transition_conf_wcsc.py
+++ lesson16/transition_conf_wcsc.py
@@ -1,44 +1,25 @@
-# States
-# ------
-#
-# ディクショナリーのキーとして Edge と被らないように PascalCase にします
-
-INIT = "Init"
-if INIT:  # インデントしてるだけ
-    LOGIN = "Login"
-
-LOBBY = "Lobby"
-if LOBBY:
-    LOGOUT = "Logout"
-
-REPLY = "Reply"
-if REPLY:
-    AGREE = "Agree"
-    REJECT = "Reject"
-
-GAME = "Game"
-if GAME:
-    pass
-
-# Edges
-# -----
-#
-# ディクショナリーのキーとして State と被らないように頭に snake_case にします
-
-E_OK = "ok"
-E_INCORRECT = "incorrect"
-E_LOGOUT = "logout"
-E_COMPLETED = "completed"
-E_GAME_SUMMARY = "game_summary"
-E_AGREE = "agree"
-E_REJECT = "reject"
-E_START = "start"
-E_MOVE = "move"
-E_MOVE_ECHO = "move_echo"
-E_GAME_OVER_FLOODGATE = "game_over_floodgate"
-E_GAME_OVER_WCSC = "game_over_wcsc"
-
-E_OVER = "over"  # 練習プログラム用
+from lesson16.const_conf import (
+    AGREE,
+    E_AGREE,
+    E_COMPLETED,
+    E_GAME_OVER_FLOODGATE,
+    E_GAME_OVER_WCSC,
+    E_GAME_SUMMARY,
+    E_INCORRECT,
+    E_LOGOUT,
+    E_MOVE,
+    E_MOVE_ECHO,
+    E_OK,
+    E_REJECT,
+    E_START,
+    GAME,
+    INIT,
+    LOBBY,
+    LOGIN,
+    LOGOUT,
+    REJECT,
+    REPLY,
+)
 
 
 class TransitionConf:
