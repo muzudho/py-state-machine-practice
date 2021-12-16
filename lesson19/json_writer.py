a762cee83bba2192ce9f13a3a783d7fae0e524d1
むずでょ <muzudho1@gmail.com>
2021-12-16 20:59:48 +0900

    JSON WIP

--- lesson19/json_writer.py
+++ lesson19/json_writer.py
@@ -3,6 +3,10 @@ import os
 
 class JsonWriter:
     @classmethod
+    def n4sp(clazz):
+        return "    "  # 4 spaces
+
+    @classmethod
     def write(clazz, file_path, data):
 
         try:
@@ -19,14 +23,20 @@ class JsonWriter:
 
     @classmethod
     def stringify(clazz, data):
-        n4sp = "    "  # 4 spaces
+        n4sp = JsonWriter.n4sp()  # 4 spaces
         text = ""
 
-        list_s = []
-
-        for k, v in data.items():
-            list_s.append(f'"{k}":{{\n{n4sp}}}')
+        list_s = JsonWriter.list_2(data)
 
         text += f"{{\n{n4sp}" + f",\n{n4sp}".join(list_s) + "\n}\n"
 
         return text
+
+    @classmethod
+    def list_2(clazz, data):
+        n4sp = JsonWriter.n4sp()  # 4 spaces
+        list_s = []
+        for k, v in data.items():
+            list_s.append(f'"{k}":{{\n{n4sp}}}')
+
+        return list_s
