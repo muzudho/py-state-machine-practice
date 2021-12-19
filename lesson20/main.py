b6be4531edf2338cecd1abd996686faafcbf7fa0
むずでょ <muzudho1@gmail.com>
2021-12-19 20:07:17 +0900

    Lesson20

--- lesson20/main.py
+++ lesson20/main.py
@@ -1,23 +1,23 @@
 import sys
 import json
+import os
 
 from lesson07n2.main_finally import MainFinally
 from lesson16.code_gen.file_io import FileIo
 from lesson19.code_gen.transition_json_writer import TransitionJsonWriter
 from lesson20.transition_json_reader import TransitionJsonReader
 
-JSON_FILE_PATH = "lesson19/auto/transition-pen.json"
+INPUT_JSON_FILE_PATH = "lesson19/step2_auto/transition-pen.json"
 
 
 class Main:
     def on_main(self):
-        transition_conf_data = TransitionJsonReader.read_file(JSON_FILE_PATH)
+        transition_conf_data = TransitionJsonReader.read_file(INPUT_JSON_FILE_PATH)
         # print(f"transition_conf_data={transition_conf_data}")
 
-        dir_path = "lesson20/auto"
         file_path = "lesson20/auto/transition-pen-default-fomat.json"
         out_text = json.dumps(transition_conf_data, indent=4, ensure_ascii=False)
-        FileIo.makedirs(dir_path)
+        FileIo.makedirs(os.path.dirname(file_path))
         print(f"[L20] write {file_path}")
         FileIo.write(file_path, out_text)
 
