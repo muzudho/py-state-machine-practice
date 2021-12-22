895884890030ea6e79f9cc87da3be0c7dfd14860
むずでょ <muzudho1@gmail.com>
2021-12-22 23:00:12 +0900

    Lesson 23

--- lesson23/README.md
+++ lesson23/README.md
@@ -3,15 +3,60 @@
 Lesson18 をさらに発展させ、  
 Pythonのdict（これはOrderedDictではありません）を経由せず、JSON（OrderedDictを使います）でデータ化した定数（`const_dict.py`）を元に、Lesson14と同等のPythonスクリプトのルーチン（状態のスクリプト）を自動生成しましょう  
 
-# Run
+# Pen
+
+## Set up
 
 ```shell
 python.exe -m lesson23.main_pen_step1
 python.exe -m lesson23.main_pen_step2
+python.exe -m lesson23.main_pen_step3
+```
 
+## Run
+
+Server start:  
+
+```shell
+python.exe -m lesson18.main_pen_step3
+```
+
+Client start:  
+
+```shell
+python.exe -m lesson09.main
+```
+
+Input:  
+
+```shell
+This
+is
+a
+pen
+q
+```
+
+# house3n2
+
+## Set up
+
+```shell
 python.exe -m lesson23.main_house3n2_step1
 python.exe -m lesson23.main_house3n2_step2
+```
 
+## Run
+
+```shell
+```
+
+# wcsc
+## Set up
+
+```shell
 python.exe -m lesson23.main_wcsc_step1
 python.exe -m lesson23.main_wcsc_step2
 ```
+
+## Run
---
+++ lesson23/main_pen_step3.py
@@ -0,0 +1,38 @@
+import sys
+
+
+from lesson07n2.main_finally import MainFinally
+from lesson18.server import Server
+from lesson14_data.step2_pen_transition import pen_transition_py_dict
+
+# Lesson 23
+from lesson23_data.step1n2_auto_const.pen_const import INIT
+from lesson23_data.pen_step4_state_gen import pen_state_gen
+
+server = None
+
+
+class Main:
+    def on_main(self):
+        server = Server(
+            state_gen=pen_state_gen,
+            transition_py_dict=pen_transition_py_dict,
+            host="0.0.0.0",
+            port=5002,
+            entry_state=INIT,
+        )
+        server.run()
+        return 0
+
+    def on_finally(self):
+        # [Ctrl] + [C] を受け付けないから、ここにくるのは難しい
+        if server:
+            server.clean_up()
+
+        print("★しっかり終わった")
+        return 1
+
+
+# このファイルを直接実行したときは、以下の関数を呼び出します
+if __name__ == "__main__":
+    sys.exit(MainFinally.run(Main()))
