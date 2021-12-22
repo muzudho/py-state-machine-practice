b2e409787ee464b89676c19c8980d4e729035c3b
むずでょ <muzudho1@gmail.com>
2021-12-22 22:42:25 +0900

    Rename

--- lesson18_data/pen_step4_state_gen.py
+++ lesson18_data/pen_step4_state_gen.py
@@ -1,10 +1,10 @@
 """State Generator"""
 from lesson18_data.step1n2_auto_const.pen_const import A, INIT, IS, PEN, THIS
-from lesson18.step3_man_state.init import create_init_state
-from lesson18.step3_man_state.init_this import create_init_this_state
-from lesson18.step3_man_state.init_this_is import create_init_this_is_state
-from lesson18.step3_man_state.init_this_is_a import create_init_this_is_a_state
-from lesson18.step3_man_state.pen import create_pen_state
+from lesson18.pen_step3_man_state.init import create_init_state
+from lesson18.pen_step3_man_state.init_this import create_init_this_state
+from lesson18.pen_step3_man_state.init_this_is import create_init_this_is_state
+from lesson18.pen_step3_man_state.init_this_is_a import create_init_this_is_a_state
+from lesson18.pen_step3_man_state.pen import create_pen_state
 from lesson18.pen_step2n2_auto_state.init_this_is_a import InitThisIsAState
 from lesson18.pen_step2n2_auto_state.init_this_is import InitThisIsState
 from lesson18.pen_step2n2_auto_state.init_this import InitThisState
