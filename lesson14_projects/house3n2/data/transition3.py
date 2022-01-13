608ca3b0f78f53d6f49d2ffcc1432a87f55b482b
むずでょ <muzudho1@gmail.com>
2022-01-13 22:58:58 +0900

    Revert "L19-house"
    This reverts commit 5105d1b6f9f1a21536127c0152cedcf782adbe94.

---
+++ lesson14_projects/house3n2/data/transition3.py
@@ -0,0 +1,40 @@
+from lesson12_projects.house3.data.const import (
+    CLOSE_DOOR,
+    E_ENTER,
+    E_PULLED_KNOB,
+    E_TURNED_KNOB,
+    OPEN_DOOR,
+    OUT,
+    E_FAILED,
+    E_SITTING_DOWN,
+    E_UP,
+    MY_ROOM,
+    STAIRS,
+)
+
+house3n2_transition3_doc_v14 = {
+    "title": "House",
+    "entry_state": OUT,
+    "data": {
+        OUT: {
+            E_TURNED_KNOB: [OUT, CLOSE_DOOR],
+            E_FAILED: [OUT],
+            CLOSE_DOOR: {
+                E_PULLED_KNOB: [OUT, OPEN_DOOR],
+                E_FAILED: [OUT],
+            },
+            OPEN_DOOR: {
+                E_ENTER: [STAIRS],
+                E_FAILED: [OUT],
+            },
+        },
+        STAIRS: {
+            E_UP: [MY_ROOM],
+            E_FAILED: [OUT],
+        },
+        MY_ROOM: {
+            E_SITTING_DOWN: None,  # None に変更
+            E_FAILED: [OUT],
+        },
+    },
+}
