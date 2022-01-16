751a4b50e21d2efceb293507cb20ad0801d51fcc
むずでょ <muzudho1@gmail.com>
2022-01-16 18:56:06 +0900

    L17

--- lesson17/code_gen/const_v17.py
+++ lesson17/code_gen/const_v17.py
@@ -1,12 +1,22 @@
 class ConstV17:
     def __init__(self, doc):
+        # (1) キーと値は 全単射にしてください
+        # (2) キーも値も 大文字と小文字は区別します
         self._doc = doc
 
         # 逆向きは自動生成します
         self._rev_data = {}
 
+        init_size = len(self._doc)
+
         for key, value in self._doc.items():
+            if value in self._rev_data:
+                raise ValueError(f"value:{value} が重複しました。全単射にしてください")
+
             self._rev_data[value] = key
+
+        if init_size != len(self._rev_data):
+            raise ValueError("定数のキーとバリューは全単射にしてください")
 
     @property
     def doc(self):
--- lesson18/code_gen/const_v18.py
+++ lesson18/code_gen/const_v18.py
@@ -1,30 +1,9 @@
-class ConstV18:
+from lesson17.code_gen.const_v17 import ConstV17
+
+
+class ConstV18(ConstV17):
     def __init__(self, doc):
-        # (1) キーと値は 全単射にしてください
-        # (2) 大文字と小文字は区別します
-        self._doc = doc
-
-        # 逆向きは自動生成します
-        self._rev_data = {}
-
-        init_size = len(self._doc)
-
-        for key, value in self._doc.items():
-            if value in self._rev_data:
-                raise ValueError(f"value:{value} が重複しました。全単射にしてください")
-
-            self._rev_data[value] = key
-
-        if init_size != len(self._rev_data):
-            raise ValueError("定数のキーとバリューは全単射にしてください")
-
-    @property
-    def doc(self):
-        return self._doc
-
-    @property
-    def rev_data(self):
-        return self._rev_data
+        super().__init__(doc)
 
     def replace_item(self, item, quote):
         """文字列に対して、定数に書きかえられるなら定数に書きかえて返します"""
