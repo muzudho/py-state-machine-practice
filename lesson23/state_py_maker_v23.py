0dbf016be50ba5e8a7445525806cfacca8a1dc94
むずでょ <muzudho1@gmail.com>
2022-01-16 13:20:21 +0900

    L23

--- lesson23/state_py_maker_v23.py
+++ lesson23/state_py_maker_v23.py
@@ -3,6 +3,7 @@ import argparse
 import traceback
 
 from lesson07n2.main_finally import MainFinally
+from lesson11n90.code_gen.toml_reader_v11n90 import TomlReaderV11n90
 from lesson11n100.code_gen.json_reader_v11n100 import JsonReaderV11n100
 from lesson18.code_gen.state_files_gen import gen_state_files_v18
 
@@ -20,31 +21,37 @@ class Main:
 
     def on_main(self):
         parser = argparse.ArgumentParser(description='ステートを定義した .pyファイルを作成します')
-        parser.add_argument('input_const', help='定数を定義した入力ファイル(.json)')
-        parser.add_argument('input_transition', help='状態遷移を定義した入力ファイル(.json)')
-        parser.add_argument('import_module', help='import文に書く文字列')
-        parser.add_argument('output', help='状態を定義したファイルを出力するディレクトリ')
+        parser.add_argument('conf',
+                            help='設定ファイル（TOML形式）へのパス')
+        parser.add_argument('const',
+                            help='定数を定義した入力ファイル（JSON形式）へのパスが入ったプロパティの名前')
+        parser.add_argument('transition',
+                            help='状態遷移を定義した入力ファイル（JSON形式）へのパスが入ったプロパティの名前')
+        parser.add_argument('import_module',
+                            help='[import_module]テーブル下の、import文のモジュールへのパスが入ったプロパティの名前')
+        parser.add_argument('output',
+                            help='状態を定義したファイルを出力するディレクトリ')
         args = parser.parse_args()
 
-        # Example: "lesson18n2_projects/house3/data/const.json"
-        print(f'args.input_const : {args.input_const}')
-        # Example: "lesson20_projects/house3n2/data/auto_gen/transition3.json"
-        print(f'args.input_transition : {args.input_transition}')
-        # Example: "lesson23_projects.house3n2.data.auto_gen.const"
-        print(f'args.import_module : {args.import_module}')
-        # Example: "lesson23/house3n2/auto_gen/states"
-        print(f'args.output : {args.output}')
+        # 設定ファイル（.toml）読取
+        toml_doc = TomlReaderV11n90.read_file(args.conf)
+
+        # TOMLの内容を読み取ります
+        const_file_path = toml_doc[args.const]
+        transition_file_path = toml_doc[args.transition]
+        import_module_path = toml_doc['import_module'][args.import_module]
+        output_graph_text_file = toml_doc[args.output]
 
         # JSONファイルから、定数と遷移の設定を読込みます
-        const_json_obj = JsonReaderV11n100.read_file(args.input_const)
-        transition_doc = JsonReaderV11n100.read_file(args.input_transition)
+        const_json_obj = JsonReaderV11n100.read_file(const_file_path)
+        transition_doc = JsonReaderV11n100.read_file(transition_file_path)
 
         # 状態の .py スクリプトを出力します
         gen_state_files_v18(
-            dir_path=args.output,
+            dir_path=output_graph_text_file,
             const_doc=const_json_obj,
             transition_doc=transition_doc,
-            import_from_path=args.import_module,
+            import_from_path=import_module_path,
         )
         return 0
 
